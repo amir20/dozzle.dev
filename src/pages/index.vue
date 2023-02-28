1dbde3cdba1941e4a61e8381079818b0cf1e4c69
Amir Raminfar <findamir@gmail.com>
2023-02-27 16:20:42 -0800

    Adds more files

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,4 +10,16 @@ defineOptions({
     <features />
     <installation />
   </main>
+
+  <router-link to="/guide/what-is-dozzle">
+    What is Dozzle?
+  </router-link>
+
+  <router-link to="/guide/getting-started">
+    Getting Started
+  </router-link>
+
+  <router-link to="/test">
+    test
+  </router-link>
 </template>
